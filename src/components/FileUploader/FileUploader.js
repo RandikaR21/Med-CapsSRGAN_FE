54d571e8d5d19728fbca239a1339294f49e60fec
RandikaR21 <josephrandika@gmail.com>
2022-04-02 09:52:45 +0530

    basic connection to the backend to get sr image

--- src/components/FileUploader/FileUploader.js
+++ src/components/FileUploader/FileUploader.js
@@ -1,9 +1,9 @@
-import React , {useRef} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCloudUpload} from "@fortawesome/free-solid-svg-icons";
+import React, { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudUpload } from "@fortawesome/free-solid-svg-icons";
 import './FileUploader.css'
 
-function FileUploader(props){
+function FileUploader(props) {
     const dropArea = useRef(null)
     const input = useRef(null)
     const dragText = useRef(null)
@@ -39,24 +39,34 @@ function FileUploader(props){
     const showFile = () => {
         let fileType = file.type; //getting selected file type
         let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-        if(validExtensions.includes(fileType)){ //if user selected file is an image file
-        }else{
+        if (validExtensions.includes(fileType)) { //if user selected file is an image file
+            const formData = new FormData()
+            formData.append("file", file)
+            fetch("http://127.0.0.1:5000/upload", {
+                method: "POST",
+                body: formData
+            }).then((response) => (response.blob()).then((blob) => {
+                console.log(blob)
+                const imageObjectURL = URL.createObjectURL(blob);
+                props.fileUpload(imageObjectURL)
+            }))
+        } else {
             alert("This is not an Image File!");
             dropArea.current.classList.remove("active");
             dragText.current.textContent = "Drag & Drop to Upload File";
         }
     }
-    return(
+    return (
         <>
             <div
                 onDragOver={dropAreaDragOver}
                 onDragLeave={dropAreaDragLeave}
                 onDrop={dropAreaDrop}
                 ref={dropArea} className={"drag-area"}>
-                <div className={"icon"}><FontAwesomeIcon icon={faCloudUpload}/></div>
+                <div className={"icon"}><FontAwesomeIcon icon={faCloudUpload} /></div>
                 <header ref={dragText}>Drag & Drop to Upload File</header>
                 <span>OR</span>
-                <button onClick = {buttonClick}>Browse File</button>
+                <button onClick={buttonClick}>Browse File</button>
                 <input onChange={inputOnchange} ref={input} type="file" hidden />
             </div>
         </>
