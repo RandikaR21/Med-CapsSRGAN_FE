aa5d6516ace4b12ed678a4360df51144e6771f75
RandikaR21 <josephrandika@gmail.com>
2022-04-02 09:46:22 +0530

    remove dumpy file handler on upload

--- src/components/FileUploader/FileUploader.js
+++ src/components/FileUploader/FileUploader.js
@@ -40,15 +40,6 @@ function FileUploader(props){
         let fileType = file.type; //getting selected file type
         let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
         if(validExtensions.includes(fileType)){ //if user selected file is an image file
-            let fileReader = new FileReader(); //creating new FileReader object
-            fileReader.onload = ()=>{
-                let fileURL = fileReader.result; //passing user file source in fileURL variable
-                props.fileUpload(fileURL)
-                // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
-                // let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
-                // dropArea.current.innerHTML = imgTag; //adding that created img tag inside dropArea container
-            }
-            fileReader.readAsDataURL(file);
         }else{
             alert("This is not an Image File!");
             dropArea.current.classList.remove("active");
