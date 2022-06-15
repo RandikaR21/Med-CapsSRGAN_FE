8a4e97d2a02560c3403a09b3ad960749b229eead
RandikaR21 <josephrandika@gmail.com>
2022-06-15 19:13:17 +0530

    http://127.0.0.1:8000/uploadfile

--- src/components/FileUploader/FileUploader.js
+++ src/components/FileUploader/FileUploader.js
@@ -71,7 +71,7 @@ function FileUploader(props){
         const formData = new FormData()
         formData.append("file", fileState)
         console.log(formData)
-        fetch("http://localhost:8000/uploadfile", {
+        fetch("http://127.0.0.1:8000/uploadfile", {
             method: "POST",
             body: formData
         }).then((response) => (response.blob()).then((blob) => {
